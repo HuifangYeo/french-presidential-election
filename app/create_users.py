f9bef5e9a31f038b2406f0b022fd056abce81932
Hui Fang Yeo <hfy@activeviam.com>
2022-04-22 10:19:18 +0800

    Atoti+ security implementation

---
+++ app/create_users.py
@@ -0,0 +1,11 @@
+import atoti as tt
+
+
+def create_users(session: tt.Session, /) -> None:
+    elon = session.security.basic.create_user("user", password="user")
+    print(f"User created with roles: {elon.roles}")
+
+    admin = session.security.basic.create_user("tt_admin", password="tt_admin")
+    session.security.individual_roles[admin.username].add("ROLE_ADMIN")
+    session.security.individual_roles[admin.username].add("ROLE_SHARE")
+    print(f"Admin created with roles: {admin.roles}")
